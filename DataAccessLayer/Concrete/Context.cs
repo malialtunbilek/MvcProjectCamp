35befa269713cd30a61f49b67a2e940f1b71936a
Mehmet Altunbilek <malialtunbilek1@gmail.com>
2021-05-28 00:14:44 +0300

    Checkpoint 1 uploaded.

---
+++ DataAccessLayer/Concrete/Context.cs
@@ -0,0 +1,20 @@
+﻿using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Concrete
+{
+    public class Context:DbContext //Buradakiler SQL'e tablo olarak yansıtılacak
+    {
+        public DbSet<About> Abouts{ get; set; }
+        public DbSet<Category> Categories{ get; set; }
+        public DbSet<Writer> Writers { get; set; }
+        public DbSet<Content> Contents  { get; set; }
+        public DbSet<Heading> Headings   { get; set; }
+        public DbSet<Contact> Contacts{ get; set; }
+    }
+}
